9279ce20490d6eadc22036731483a4fed9bd25d6
plouuf <therandomploouf@protonmail.com>
2021-02-19 16:30:42 -0330

    db setup

--- Server/utils/db.js
+++ Server/utils/db.js
@@ -3,13 +3,13 @@ const uri ="mongodb://localhost:27017";
 const client = new MongoClient(uri, { useUnifiedTopology: true });
 
 /**
- * A function to stablish a connection with a MongoDB instance.
+ * A function to establish a connection with a MongoDB instance.
  */
 async function connectToDB() {
     try {
         // Connect the client to the server
         await client.connect();
-        // Our db name is going to be my-library
+        // Our db name is going to be FastMigrate
         let db = client.db('FastMigrate');
         console.log("Successfully connected to mongoDB");  
         return db;
