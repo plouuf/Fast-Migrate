e98351531804934b0c2db3d8a3545f8ad30262cb
plouuf <therandomploouf@protonmail.com>
2021-02-21 18:06:10 -0330

    dataset added

--- Server/app.js
+++ Server/app.js
@@ -1,23 +1,18 @@
-const express = require("express")
-const country_router = require('./routes/country')
-const user_router = require('./routes/users')
-const port = 3000
+const express = require('express');
+const country_router = require('./routes/country');
+const user_router = require('./routes/users');
+const port = 3000;
 
-const app = express()
+const app = express();
 
-
-
-app.use(express.json())
-app.use('/country', country_router)
-app.use('/user', user_router)
+app.use(express.json());
+app.use('/country', country_router);
+app.use('/user', user_router);
 
 app.get('/', (req, res) => {
   res.send('Homepage :)');
 });
 
-
 server = app.listen(port, () => {
-	console.log(`App listening at http://localhost:${port}`);
+  console.log(`App listening at http://localhost:${port}`);
 });
-  
-
--- Server/utils/db.js
+++ Server/utils/db.js
@@ -1,34 +1,33 @@
-const MongoClient = require("mongodb").MongoClient
-const uri ="mongodb://localhost:27017";
+const MongoClient = require('mongodb').MongoClient;
+const uri = 'mongodb://localhost:27017';
 const client = new MongoClient(uri, { useUnifiedTopology: true });
 
 /**
  * A function to establish a connection with a MongoDB instance.
  */
 async function connectToDB() {
-    try {
-        // Connect the client to the server
-        await client.connect();
-        // Our db name is going to be FastMigrate
-        let db = client.db('FastMigrate');
-        console.log("Successfully connected to mongoDB");  
-        return db;
-    } catch (err) {
-        throw err;
-    } 
+  try {
+    // Connect the client to the server
+    await client.connect();
+    // Our db name is going to be FastMigrate
+    let db = client.db('FastMigrate');
+    console.log('Successfully connected to mongoDB');
+    return db;
+  } catch (err) {
+    throw err;
+  }
 }
 
 async function getDb() {
-    return db;
+  return db;
 }
 
-async function closeDBConnection(){
-    try{
-        await client.close();    
-    }catch(err){
-        throw err;
-    }    
-};
+async function closeDBConnection() {
+  try {
+    await client.close();
+  } catch (err) {
+    throw err;
+  }
+}
 
-
-module.exports = {connectToDB, getDb, closeDBConnection}
+module.exports = { connectToDB, getDb, closeDBConnection };
