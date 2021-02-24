b6e2a7f839118f4856f54c2a213232caead74cdf
plouuf <therandomploouf@protonmail.com>
2021-02-24 17:04:12 -0330

    added mongo utils

--- Server/app.js
+++ Server/app.js
@@ -1,25 +1,26 @@
 const express = require('express');
 const country_router = require('./routes/country');
 const user_router = require('./routes/users');
+const mongo = require('./utils/db');
 const port = 3000;
 
-// var db;
-// async function loadDBClient() {
-// 	try {
-// 		db = await mongo.connectToDB();
-// 	}catch(err){
-// 		throw new Error('Could not connect to the Mongo DB');
-// 	}
-// };  
+var db;
+async function loadDBClient() {
+	try {
+		db = await mongo.connectToDB();
+	}catch(err){
+		throw new Error('Could not connect to the Mongo DB');
+	}
+};  
 
-// loadDBClient();
+loadDBClient();
 
 const app = express();
 
-// app.use((req, res, next) => {
-// 	req.db = db;	
-// 	next();
-// });
+app.use((req, res, next) => {
+	req.db = db;	
+	next();
+});
 
 app.use(express.json());
 app.use('/country', country_router);
@@ -33,11 +34,11 @@ server = app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
 });
 
-// process.on('SIGINT', () => {
-// 	console.info('SIGINT signal received.');
-// 	console.log('Closing Mongo Client.');
-// 	mongo.closeDBConnection();
-// 	server.close(() => {
-// 	  console.log('Http server closed.');
-// 	});
-//  });
+process.on('SIGINT', () => {
+	console.info('SIGINT signal received.');
+	console.log('Closing Mongo Client.');
+	mongo.closeDBConnection();
+	server.close(() => {
+	  console.log('Http server closed.');
+	});
+ });
