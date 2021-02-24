11fd8fd4367ed5cce49e9aba53d038b9af28560e
ehlhasoon <55723249+ehlhasoon@users.noreply.github.com>
2021-02-24 19:36:47 -0330

    info api

--- Server/utils/addCountryInfoAPI.js
+++ Server/utils/addCountryInfoAPI.js
@@ -5,7 +5,13 @@ getCountryInfo = (country_name) => {
         let url = 'https://www.numbeo.com/api/';
         let key = 'x9ey41xq1xqezv';
         let country_api = axios.get(url+"/api/country_indices?api_key="+key+"&country="+country_name)
-                            .then(res => { return res.data.main;})
+                            .then(res => { 
+                                let health_care = res.data.results[0];
+                                let crime_index = res.data.results[1];
+                                let cost_of_living = res.data.results[4];
+                                let quality_of_life = res.data.results[7];                                
+                                return [health_care, crime_index, cost_of_living, quality_of_life];
+                            })
                             .catch( err => {
                                 console.log(err);
                                 return null;
