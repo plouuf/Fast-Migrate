cefb14f99b0429567b03778c18cfb21241a10253
plouuf <therandomploouf@protonmail.com>
2021-02-25 14:07:43 -0330

    country model. Need to add unemployment_rate..

--- Server/models/country.js
+++ Server/models/country.js
@@ -10,11 +10,11 @@ class Country {
   constructor(name) {
     this.name = name;
   }
-
+  
   addCountryInfo(happiness_score, Gdp, crime_index, quality_of_life, health_care, cost_of_living) {
     this.happiness_score = happiness_score;
     this.Gdp = Gdp;
-    this.unemployment_rate = unemployment_rate;
+    // this.unemployment_rate = unemployment_rate;
     this.crime_index = crime_index;
     this.quality_of_life = quality_of_life;
     this.health_care = health_care;
@@ -23,33 +23,44 @@ class Country {
 
   async save(db) {
 		var country =  this;
-		return new Promise(async function (resolve, reject){
+		return new Promise(async function (resolve, reject) {
 			let collection = await _get_country_collection(db);
       collection.insertOne(country, (err, obj) => {
         if (!err) {
-          console.log("1 document added.");
-          resolve("Country correctly inserted in the Database!");
+          console.log('1 document added.');
+          resolve('Country correctly inserted in the Database!');
         } else {
-          console.log("document not inserted.");
-          reject("Country not inserted in the database.");
+          console.log('document not inserted.');
+          reject('Country not inserted in the database.');
         }
       });	
 		});
 	};
 
 	static async update(db, name, happiness_score, Gdp, unemployment_rate, crime_index, quality_of_life, health_care, cost_of_living) {
-		return new Promise(async function (resolve, reject){
+		return new Promise(async function (resolve, reject) {
 			let collection = await _get_country_collection(db);
-			let new_vals = {$set: {'happiness_score': happiness_score, 'Gdp': Gdp, 'unemployment_rate': unemployment_rate, 'crime_index': crime_index, 'quality_of_life' : quality_of_life, 'health_care' : health_care, 'cost_of_living' : cost_of_living}};
+      let new_vals = {
+        $set: {
+          'happiness_score': happiness_score,
+          'Gdp': Gdp,
+          'unemployment_rate': unemployment_rate,
+          'crime_index': crime_index,
+          'quality_of_life': quality_of_life,
+          'health_care': health_care,
+          'cost_of_living': cost_of_living
+        }
+      };
 
 			collection.updateOne({'name': name}, new_vals, (err, obj) => {
-				if (err) {
-					console.log("The country with name = "+name+" was not updated in the database.");
-					reject('Error: Country not updated in database');
-				}
-				
-				resolve('Country correctly updated in database');
-				console.log("The country with name = "+name+" was correctly updated in the database.");
+        if (err) throw err;
+        if (obj.modifiedCount > 0) {
+          console.log("1 document updated.");
+          resolve("Country correctly updated!");
+        } else {
+          console.log("document not updated.");
+          reject(`The Country with name ${name} is not valid!`);
+        } 
 			});
 		});
 	};
@@ -60,13 +71,14 @@ class Country {
 			let collection = await _get_country_collection(db);
 
       collection.deleteOne({'name': country_delete}, (err, obj) => {
-				if (err) {
-					console.log("The country with name = "+country_delete+" was not deleted in the database.")
-					reject('Error: Country not deleted in database');
-				}
-
-				resolve('Country correctly deleted in database');
-				console.log("The country with name = "+country_delete+" was correctly deleted in the database.");
+        if (err) throw err;
+        if (obj.result.n > 0) {
+          console.log('1 document deleted.');
+          resolve('Country deleted!');
+        } else {
+          console.log('document not deleted.');
+          reject(`Country ${country_delete} was not found!`);
+        }
 			});
 		});
 	};
@@ -75,36 +87,30 @@ class Country {
     var name_get = name;
 		return new Promise(async function (resolve, reject){
 			let collection = await _get_country_collection(db);
-
-      collection.find({"name": name}).toArray((err, items)=> {
-				if (err) {
-					console.log("The country with name = "+country_get+" was not found in the database.");
-					reject('Error: No country with name = '+country_get+' in database.');
-				}
-
-				resolve(items);
-				console.log("The country with name = "+country__get+" was found in the database.");
+      collection.find({"name": name}).toArray((err, items) => {
+				if (err) throw err;
+        if (items.length > 0) {
+					console.log(`${items.length} item(s) sent.`);
+          resolve(items);
+        } else {
+          console.log("Country was not found");
+          reject(`There is no country with name: ${name_get}.`);
+        }
 			});
-
     });
 	};
 
 	static async getCountries(db) {
 		return new Promise(async function (resolve, reject){
 			let collection = await _get_country_collection(db);
-
       collection.find({}).toArray((err, items) => {
-				if (err) {
-					console.log('No countries were retrieved from the database.');
-					reject('Error: No countries retrieved from database');
-				}
-				
-				if(items.length == 0) {
-					console.log("Database is empty");
-				}
-
-				resolve(items);
-				console.log(items.length+" countries sent.");
+				if (err) throw err;
+				if (items.length > 0) {
+					console.log(`${items.length} item(s) sent.`);
+          resolve(items);
+        } else {
+          reject("Database is empty!");
+        }
       });
     });
 	};
