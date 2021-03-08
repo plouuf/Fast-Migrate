690c66f110a7742ac76b19137d043d229cb8d21e
plouuf <therandomploouf@protonmail.com>
2021-03-07 21:10:37 -0330

    Tests done! made some minor modification in the utils functions

--- Server/test/main_test.js
+++ Server/test/main_test.js
@@ -19,8 +19,9 @@ before(async function () {
 });
 // this method will close your connection to MongoDB after the tests
 after(async function () {
-  // await Country.delete(db, 'Canada');
-  // await Country.delete(db, 'Finland');
+  //deleting countries added in simple cases now since needed to check retrieval of more than 1 country 🙂
+  await Country.delete(db, 'Canada');
+  await Country.delete(db, 'Finland');
   try {
     mongo.closeDBConnection();
   } catch (err) {
@@ -55,7 +56,6 @@ describe('Testing the Fast Migrate API', function(){
         assert.strictEqual(c.crime_index, crime_index);
         assert.strictEqual(c.health_care_index, health_care_index);
         assert.strictEqual(c.cost_of_living, cpi_index);
-
         });
         it('Fail 1 - Test an invalid Country name', async function(){
           
@@ -77,7 +77,6 @@ describe('Testing the Fast Migrate API', function(){
 
           c1.addCountryInfo(happiness, gdp, unemployment, crime_index, quality_of_life, health_care_index, cpi_index);
           assert.strictEqual(c1.isValid(), false);
-
         });
         it('Fail 2 - Test an invalid Country happiness', async function(){
           
@@ -99,7 +98,6 @@ describe('Testing the Fast Migrate API', function(){
 
           c2.addCountryInfo(happiness, gdp, unemployment, crime_index, quality_of_life, health_care_index, cpi_index);
           assert.strictEqual(c2.isValid(), false);
-
         });
         it('Fail 3 - Test an invalid Country gdp', async function(){
           
@@ -121,7 +119,6 @@ describe('Testing the Fast Migrate API', function(){
 
           c3.addCountryInfo(happiness, gdp, unemployment, crime_index, quality_of_life, health_care_index, cpi_index);
           assert.strictEqual(c3.isValid(), false);
-
         });
         it('Fail 4 - Test an invalid Country unemployment', async function(){
           
@@ -143,7 +140,6 @@ describe('Testing the Fast Migrate API', function(){
 
           c4.addCountryInfo(happiness, gdp, unemployment, crime_index, quality_of_life, health_care_index, cpi_index);
           assert.strictEqual(c4.isValid(), false);
-
         });
         it('Success 2 - Test the insertion of a valid Country (Country.save) - Success Msg test', async function(){
           
@@ -171,7 +167,6 @@ describe('Testing the Fast Migrate API', function(){
           }).catch(msg => { 
             console.log(`Error: ${msg}`);
           });
-          
         });
         it('Success 3 - Test the update of a valid Country (Country.update) - Success Msg test', async function(){
           
@@ -185,7 +180,6 @@ describe('Testing the Fast Migrate API', function(){
           }).catch(msg => { 
             console.log(`Error: ${msg}`);
           });
-
         });
         it('Success 4 - Test the deletetion of a valid Country (Country.delete) - Success Msg test', async function(){
           await Country.delete(db, 'Canada')
@@ -227,7 +221,6 @@ describe('Testing the Fast Migrate API', function(){
           }).catch(msg => { 
             console.log(`Error: ${msg}`);
           });
-
           });
         it('Success 6 - Test the retrieval of all Countries (Country.getCountries) - Success Msg test', async function(){
           
@@ -250,16 +243,16 @@ describe('Testing the Fast Migrate API', function(){
           c7.addCountryInfo(happiness, gdp, unemployment, crime_index, quality_of_life, health_care_index, cpi_index);
           await c7.save(db);
           await Country.getCountries(db).then(msg => { 
+            //should return 3 countries, two added in simple cases and one posted here 😃
             assert.strictEqual(msg.length > 1, true);
           }).catch(msg => { 
             console.log(`Error: ${msg}`);
           });
-
         });
     });
     describe('Testing the Book API - Complex Cases', function(){
         var myurl = 'http://localhost:3000/country'
-        it('Success 1 - POST /country, DELETE /country/name', function(){
+        it('Success 1 - POST /country, DELETE /country/name', function(done){
           let country_data = {
             name: 'Denmark'
           }
@@ -273,11 +266,12 @@ describe('Testing the Fast Migrate API', function(){
                 headers: { 'content-type': 'application/json' },
                 url: myurl + '/' + country_data.name
               }, function (error, response, body) { 
-                assert.strictEqual(body, 'Country deleted!');
+                  assert.strictEqual(body, 'Country deleted!');
+                  done();
               });
           });
         });
-        it('Success 2 - POST /country, GET /country (retrieval greater than 1), DELETE /country/:name', function(){
+        it('Success 2 - POST /country, GET /country (retrieval greater than 1), DELETE /country/:name', function(done){
           let country_data1 = {
             name: 'Norway'
           }
@@ -298,21 +292,77 @@ describe('Testing the Fast Migrate API', function(){
                   url: myurl + '/' + country_data1.name
                 }, function (error, response, body) { 
                     assert.strictEqual(body, 'Country deleted!');
+                    done();
                 });  
             });  
           });
         });
-        it('Success 3 - POST /books, GET /books/:id, DELETE /book/:id', function(){
-            
+        it('Success 3 - POST /country, GET /country/:name, DELETE /country/:name', function(done){
+          let country_data2 = {
+            name: 'Iceland'
+          }
+          request.post({
+            headers: { 'content-type': 'application/json' },
+            url: myurl + '/',
+            body: JSON.stringify(country_data2)
+          }, function (error, response, body) { 
+              assert.strictEqual(body, 'Country correctly inserted in the Database!');
+              request.get({
+                headers: { 'content-type': 'application/json' },
+                url: myurl + '/' + country_data2.name
+              }, function (error, response, body) {
+                let d = JSON.parse(body);
+                assert.strictEqual(d.length == 1, true);
+                request.delete({
+                  headers: { 'content-type': 'application/json' },
+                  url: myurl + '/' + country_data2.name
+                }, function (error, response, body) {
+                    assert.strictEqual(body, 'Country deleted!');
+                    done();
+                });
+              });
+          });
         });
-        it('Success 4 - POST /books, PUT /books/:id, GET /books/:id, DELETE /book/:id', function(){
-            
-        });
-        it('Success 5 - (Optional) Open', function(){
-
-        });
-        it('Success 6 - (Optional) Open', function(){
-
+        it('Success 4 - POST /country, PUT /country/:name, GET /country/:name, DELETE /country/:name', function(done){
+          let country_data3 = {
+            name: 'Netherlands'
+          }
+          let up_data = {
+            happiness_score: 8,
+            Gdp: 900,
+            unemployment_rate: 3
+          }
+          request.post({
+            headers: { 'content-type': 'application/json' },
+            url: myurl + '/',
+            body: JSON.stringify(country_data3)
+          }, function (error, response, body) { 
+              assert.strictEqual(body, 'Country correctly inserted in the Database!');
+              request.put({
+                headers: { 'content-type': 'application/json' },
+                url: myurl + '/' + country_data3.name,
+                body: JSON.stringify(up_data),
+              }, function(error, response, body){
+                  assert.strictEqual(body, 'Country correctly updated!');
+                  request.get({
+                    headers: { 'content-type': 'application/json' },
+                    url: myurl + '/' + country_data3.name
+                  }, function (error, response, body) {
+                    let d = JSON.parse(body);
+                      assert.strictEqual(d[0].name, country_data3.name);
+                      assert.strictEqual(d[0].happiness_score, up_data.happiness_score);
+                      assert.strictEqual(d[0].Gdp, up_data.Gdp);
+                      assert.strictEqual(d[0].unemployment_rate, up_data.unemployment_rate);
+                    request.delete({
+                      headers: { 'content-type': 'application/json' },
+                      url: myurl + '/' + country_data3.name
+                    }, function (error, response, body) {
+                        assert.strictEqual(body, 'Country deleted!');
+                        done();
+                    });
+                  });
+              });
+          });
         });
     });
 });
