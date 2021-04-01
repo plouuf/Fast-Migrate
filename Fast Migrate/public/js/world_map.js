1a9e899956a9f8bb85cd061e0a4b47f55f0f93c3
Moe <mohammad.hasan8622@gmail.com>
2021-03-31 21:31:04 -0230

    commit on master

--- Fast Migrate/public/js/world_map.js
+++ Fast Migrate/public/js/world_map.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
         L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=' + mapboxAccessToken, {
             id: 'mapbox/light-v10',
             attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-            maxZoom: 5,
+            maxZoom: 3,
             tileSize: 512,
             zoomOffset: -1,
             accessToken: mapboxAccessToken
